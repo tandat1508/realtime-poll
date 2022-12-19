b5b36151f1b64a8c1b94192e079bee19435dcef6
tandat <tandat@vn-cubesystem.com>
2022-12-19 15:41:59 +0900

    Updates

---
+++ precache-manifest.d77719a9030815b06eabcde143ab0a81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1f832b5505b977b3b80eac23fb648c",
+    "url": "/realtime-poll/index.html"
+  },
+  {
+    "revision": "c0f98f6c776c51949ff8",
+    "url": "/realtime-poll/static/css/main.2aa09763.chunk.css"
+  },
+  {
+    "revision": "0cadbddf5234f50f07b3",
+    "url": "/realtime-poll/static/js/2.7b4b13dc.chunk.js"
+  },
+  {
+    "revision": "24e24bd65d29a5ee59278434217dd33b",
+    "url": "/realtime-poll/static/js/2.7b4b13dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f98f6c776c51949ff8",
+    "url": "/realtime-poll/static/js/main.d5311ba2.chunk.js"
+  },
+  {
+    "revision": "ff275e5941820b66fc3f",
+    "url": "/realtime-poll/static/js/runtime-main.d061e555.js"
+  },
+  {
+    "revision": "c3351f894cc587ff7b9c43ba48c25cb3",
+    "url": "/realtime-poll/static/media/logo-white.c3351f89.svg"
+  }
+]);
